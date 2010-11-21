e4552ab3407bc24243a3bc4d5fafcefadfa626d9
bartbru <bart.bruynooghe@gmail.com>
2010-11-21 23:43:25 +0100

    compiled files should not be here

--- BeTrains/.Simulator-Debug/src/Controller/IRoutePlannerController.d
+++
@@ -1,6 +0,0 @@
-src/Controller/IRoutePlannerController.d \
- src/Controller/IRoutePlannerController.o: \
- ../src/Controller/IRoutePlannerController.cpp \
- C:/bada/1.1.0b1/IDE/workspace/BeTrains/inc/Controller/IRoutePlannerController.h
-
-C:/bada/1.1.0b1/IDE/workspace/BeTrains/inc/Controller/IRoutePlannerController.h:
--- BeTrains/.Simulator-Debug/src/View/IView.d
+++
@@ -1,4 +0,0 @@
-src/View/IView.d src/View/IView.o: ../src/View/IView.cpp \
- C:/bada/1.1.0b1/IDE/workspace/BeTrains/inc/View/IView.h
-
-C:/bada/1.1.0b1/IDE/workspace/BeTrains/inc/View/IView.h:
