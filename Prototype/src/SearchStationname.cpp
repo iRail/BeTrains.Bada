f68b450a91d7a575ebbe105376a092e80308304b
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2010-11-24 18:46:44 +0100

    wth

---
+++ Prototype/src/SearchStationname.cpp
@@ -0,0 +1,46 @@
+
+#include "Search_Station_Name.h"
+
+using namespace Osp::Base;
+using namespace Osp::Ui;
+using namespace Osp::Ui::Controls;
+
+
+Search_Station_Name::Search_Station_Name(void)
+{
+}
+
+Search_Station_Name::~Search_Station_Name(void)
+{
+}
+
+bool
+Search_Station_Name::Initialize()
+{
+	Form::Construct(L"IDF_SEARCH_STATION");
+
+	return true;
+}
+
+result
+Search_Station_Name::OnInitializing(void)
+{
+	result r = E_SUCCESS;
+
+	// TODO: Add your initialization code here
+
+
+	return r;
+}
+
+result
+Search_Station_Name::OnTerminating(void)
+{
+	result r = E_SUCCESS;
+
+	// TODO: Add your termination code here
+
+	return r;
+}
+
+
