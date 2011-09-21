35e47a5e39598db02a738983b79d4771266c83cc
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2011-09-21 14:09:04 +0200

    I18N in the rest of the application.

--- BeTrains/src/view/LiveBoardResults.cpp
+++ BeTrains/src/view/LiveBoardResults.cpp
@@ -9,13 +9,26 @@ using namespace Osp::Ui::Controls;
 using namespace Osp::Graphics;
 
 LiveBoardResults::LiveBoardResults(void) {
-	liveBoardRequest=null;
+	liveBoardRequest = null;
 }
 
 LiveBoardResults::~LiveBoardResults(void) {
 }
 
 bool LiveBoardResults::Initialize() {
+
+	/*
+	 * I18N
+	 */
+	String previous = "Previous";
+	String next = "Next";
+	String empty = "Empty list";
+	String saveToCalendar = "Save to calendar";
+	AppResource* appRes = Application::GetInstance()->GetAppResource();
+	appRes->GetString(L"LB_RES_PREV", previous);
+	appRes->GetString(L"LB_RES_NEXT", next);
+	appRes->GetString(L"LB_RES_EMPTY",empty);
+
 	this->RemoveAllControls();
 	HeaderForm::Initialize(true, true); //enables left and right softkey
 	/*
@@ -23,13 +36,13 @@ bool LiveBoardResults::Initialize() {
 	 */
 	liveBoardRequest = AppData::GetInstance()->getCurrentLiveBoardRequest(); // no ownership offcourse
 
-	this->SetSoftkeyText(SOFTKEY_0, "Previous");
+	this->SetSoftkeyText(SOFTKEY_0, previous);
 	this->SetSoftkeyActionId(SOFTKEY_0, PREVIOUS_ACTION);
-	this->AddSoftkeyActionListener(SOFTKEY_0,*this);
+	this->AddSoftkeyActionListener(SOFTKEY_0, *this);
 
-	this->SetSoftkeyText(SOFTKEY_1, "Next");
+	this->SetSoftkeyText(SOFTKEY_1, next);
 	this->SetSoftkeyActionId(SOFTKEY_1, NEXT_ACTION);
-	this->AddSoftkeyActionListener(SOFTKEY_1,*this);
+	this->AddSoftkeyActionListener(SOFTKEY_1, *this);
 
 	/*
 	 * Calculate sizes for all controls
@@ -40,8 +53,8 @@ bool LiveBoardResults::Initialize() {
 	 * Construct a ListView
 	 */
 	list = new ListView();
-	list->Construct(Rectangle(0,0,bounds.width,bounds.height),true,false);
-	list->SetTextOfEmptyList("empty list");
+	list->Construct(Rectangle(0, 0, bounds.width, bounds.height), true, false);
+	list->SetTextOfEmptyList(empty);
 	list->SetItemProvider(*this);
 	list->AddListViewItemEventListener(*this);
 	AddControl(*list);
@@ -49,16 +62,22 @@ bool LiveBoardResults::Initialize() {
 	return true;
 }
 
-
 /*
  * implements IListViewItemEventListener
  */
-void LiveBoardResults::OnListViewItemStateChanged(Osp::Ui::Controls::ListView &listView, int index, int elementId, Osp::Ui::Controls::ListItemStatus status){}
+void LiveBoardResults::OnListViewItemStateChanged(
+		Osp::Ui::Controls::ListView &listView, int index, int elementId,
+		Osp::Ui::Controls::ListItemStatus status) {
+}
 
-void LiveBoardResults::OnListViewItemSwept(Osp::Ui::Controls::ListView &listView, int index, Osp::Ui::Controls::SweepDirection direction){}
+void LiveBoardResults::OnListViewItemSwept(
+		Osp::Ui::Controls::ListView &listView, int index,
+		Osp::Ui::Controls::SweepDirection direction) {
+}
 
-void LiveBoardResults::OnListViewContextItemStateChanged(Osp::Ui::Controls::ListView &listView, int index, int elementId, Osp::Ui::Controls::ListContextItemStatus state)
-{
+void LiveBoardResults::OnListViewContextItemStateChanged(
+		Osp::Ui::Controls::ListView &listView, int index, int elementId,
+		Osp::Ui::Controls::ListContextItemStatus state) {
 	//TODO make context menu to add to calendar
 }
 
@@ -66,18 +85,17 @@ void LiveBoardResults::OnListViewContextItemStateChanged(Osp::Ui::Controls::List
  * implements IListViewItemProvider
  */
 Osp::Ui::Controls::ListItemBase*
-LiveBoardResults::CreateItem(int index, int itemWidth)
-{
+LiveBoardResults::CreateItem(int index, int itemWidth) {
 	Rectangle bounds = this->GetClientAreaBounds();
-	int itemHeight = bounds.height/8;
-	if(bounds.width>bounds.height)
-		itemHeight = bounds.width /8;
+	int itemHeight = bounds.height / 8;
+	if (bounds.width > bounds.height)
+		itemHeight = bounds.width / 8;
 
 	/*
 	 * Get the trip from the index id
 	 */
 	LiveBoardResult* result;
-	liveBoardRequest->getResults()->GetAt(index,result);
+	liveBoardRequest->getResults()->GetAt(index, result);
 	/*
 	 * Get trip data
 	 */
@@ -88,32 +106,47 @@ LiveBoardResults::CreateItem(int index, int itemWidth)
 
 	//create custom item with right size, height is calculated with amount of connections
 	CustomItem* item = new CustomItem();
-	item->Construct(Osp::Graphics::Dimension(itemWidth,itemHeight), LIST_ANNEX_STYLE_NORMAL); //+itemHeight*amountConnections
-	item->AddElement(Rectangle(0				, 0					, itemWidth*0.75	, 0.5*itemHeight), STATION		, station	, 0.5*itemHeight	, Color::COLOR_WHITE	, Color::COLOR_WHITE	, Color::COLOR_WHITE);
-	item->AddElement(Rectangle(0				, 0.5*itemHeight	, itemWidth*0.75	, 0.5*itemHeight), TIME			, time		, 0.5*itemHeight	, Color::COLOR_WHITE	, Color::COLOR_WHITE	, Color::COLOR_WHITE);
-	item->AddElement(Rectangle(itemWidth*0.75	, 0					, itemWidth*0.25	, 0.5*itemHeight), PLATFORM		, platform	, 0.5*itemHeight	, Color::COLOR_WHITE	, Color::COLOR_BLUE		, Color::COLOR_BLUE);
-	item->AddElement(Rectangle(itemWidth*0.75	, 0.5*itemHeight	, itemWidth*0.25	, 0.5*itemHeight), DELAY		, delay  	, 0.5*itemHeight	, Color::COLOR_RED		, Color::COLOR_BLUE		, Color::COLOR_BLUE);
+	item->Construct(Osp::Graphics::Dimension(itemWidth, itemHeight),
+			LIST_ANNEX_STYLE_NORMAL); //+itemHeight*amountConnections
+	item->AddElement(Rectangle(0, 0, itemWidth * 0.75, 0.5 * itemHeight),
+			STATION, station, 0.5 * itemHeight, Color::COLOR_WHITE,
+			Color::COLOR_WHITE, Color::COLOR_WHITE);
+	item->AddElement(Rectangle(0, 0.5 * itemHeight, itemWidth * 0.75, 0.5
+			* itemHeight), TIME, time, 0.5 * itemHeight, Color::COLOR_WHITE,
+			Color::COLOR_WHITE, Color::COLOR_WHITE);
+	item->AddElement(Rectangle(itemWidth * 0.75, 0, itemWidth * 0.25, 0.5
+			* itemHeight), PLATFORM, platform, 0.5 * itemHeight,
+			Color::COLOR_WHITE, Color::COLOR_BLUE, Color::COLOR_BLUE);
+	item->AddElement(Rectangle(itemWidth * 0.75, 0.5 * itemHeight, itemWidth
+			* 0.25, 0.5 * itemHeight), DELAY, delay, 0.5 * itemHeight,
+			Color::COLOR_RED, Color::COLOR_BLUE, Color::COLOR_BLUE);
+
+	/*
+	 * I18N
+	 */
+	String saveToCalendar = "Save to calendar";
+	AppResource* appRes = Application::GetInstance()->GetAppResource();
+	appRes->GetString(L"LB_RES_TO_CAL",saveToCalendar);
 
 	/*
 	 * create context menu
 	 */
 	ListContextItem* itemContext = new ListContextItem();
 	itemContext->Construct();
-	itemContext->AddElement(SAVE_CALENDAR_ACTION, "Save to calendar");
+	itemContext->AddElement(SAVE_CALENDAR_ACTION, saveToCalendar);
 	item->SetContextItem(itemContext);
 
 	return item;
 }
 
-bool
-LiveBoardResults::DeleteItem(int index, Osp::Ui::Controls::ListItemBase* item, int itemWidth)
-{
-    delete item; item = null; return true;
+bool LiveBoardResults::DeleteItem(int index,
+		Osp::Ui::Controls::ListItemBase* item, int itemWidth) {
+	delete item;
+	item = null;
+	return true;
 }
 
-int
-LiveBoardResults::GetItemCount(void)
-{
+int LiveBoardResults::GetItemCount(void) {
 	return liveBoardRequest->getResults()->GetCount();
 }
 
@@ -131,18 +164,18 @@ result LiveBoardResults::OnTerminating(void) {
 /*
  * IAction Event Listener
  */
-void LiveBoardResults::OnActionPerformed(const Osp::Ui::Control& source,int actionId) {
-	HeaderForm::OnActionPerformed(source,actionId);
-	if(actionId == NEXT_ACTION){
+void LiveBoardResults::OnActionPerformed(const Osp::Ui::Control& source,
+		int actionId) {
+	HeaderForm::OnActionPerformed(source, actionId);
+	if (actionId == NEXT_ACTION) {
 		AppLog("Clicked LiveBoardResults::search");
 		Controller::GetInstance()->retrieveLiveBoardResults();
-	}else if(actionId == PREVIOUS_ACTION){
+	} else if (actionId == PREVIOUS_ACTION) {
 		AppLog("Clicked LiveBoardResults::clear");
 	}
 }
 
-
-void LiveBoardResults::RequestRedraw (bool show) const{
+void LiveBoardResults::RequestRedraw(bool show) const {
 	AppLog("LiveBoardResults::RequestRedraw");
 	Form::RequestRedraw(show);
 }
--- BeTrains/src/view/RoutePlannerResults.cpp
+++ BeTrains/src/view/RoutePlannerResults.cpp
@@ -8,11 +8,8 @@ using namespace Osp::Ui;
 using namespace Osp::Ui::Controls;
 using namespace Osp::Graphics;
 
-RoutePlannerResults::RoutePlannerResults(void):
-	request(null),
-	format(null),
-	subListFormat(null)
-{
+RoutePlannerResults::RoutePlannerResults(void) :
+	request(null), format(null), subListFormat(null) {
 }
 
 RoutePlannerResults::~RoutePlannerResults(void) {
@@ -21,46 +18,63 @@ RoutePlannerResults::~RoutePlannerResults(void) {
 }
 
 bool RoutePlannerResults::Initialize() {
+
+	/*
+	 * I18N
+	 */
+	String refresh = "Refresh";
+	String back = "Back";
+	String empty = "Empty List";
+	String saveToCalendar = "Save to calendar";
+	String direct = "Direct";
+	AppResource* appRes = Application::GetInstance()->GetAppResource();
+	appRes->GetString(L"RP_RES_REFRESH", refresh);
+	appRes->GetString(L"RP_RES_BACK", back);
+	appRes->GetString(L"RP_RES_EMPTY", empty);
+	appRes->GetString(L"RP_RES_SAVE_TO_CAL", saveToCalendar);
+
 	this->RemoveAllControls();
 	HeaderForm::Initialize(true, true); //enables left and right softkey
-	this->request=AppData::GetInstance()->getCurrentRequest();
+	this->request = AppData::GetInstance()->getCurrentRequest();
 	/*
 	 * get Data from current request from appdata
 	 */
 	request = AppData::GetInstance()->getCurrentRequest(); // no ownership offcourse
 
-	this->SetSoftkeyText(SOFTKEY_0, "Refresh");
+	this->SetSoftkeyText(SOFTKEY_0, refresh);
 	this->SetSoftkeyActionId(SOFTKEY_0, REFRESH_ACTION);
-	this->AddSoftkeyActionListener(SOFTKEY_0,*this);
+	this->AddSoftkeyActionListener(SOFTKEY_0, *this);
 
-	this->SetSoftkeyText(SOFTKEY_1, "Back");
+	this->SetSoftkeyText(SOFTKEY_1, back);
 	this->SetSoftkeyActionId(SOFTKEY_1, BACK_ACTION);
-	this->AddSoftkeyActionListener(SOFTKEY_1,*this);
+	this->AddSoftkeyActionListener(SOFTKEY_1, *this);
 
 	/*
 	 * Calculate sizes for all controls
 	 */
 	Rectangle bounds = this->GetClientAreaBounds();
-	int titleHeight = bounds.height/12;
-	int height = bounds.height/7;
-	if(bounds.width>bounds.height)
-		height = bounds.width /7;
+	int titleHeight = bounds.height / 12;
+	int height = bounds.height / 7;
+	if (bounds.width > bounds.height)
+		height = bounds.width / 7;
 	int width = bounds.width;
 
 	/*
 	 * Construct title label
 	 */
 	titleLabel = new Label();
-	titleLabel->Construct(Rectangle(0, 0, bounds.width, titleHeight), L"station - station");
+	titleLabel->Construct(Rectangle(0, 0, bounds.width, titleHeight),
+			L"station - station");
 	AddControl(*titleLabel);
 
 	/*
 	 * Construct a ListView
 	 */
 	list = new ExpandableList();
-	list->Construct(Rectangle(0,titleHeight,bounds.width,bounds.height-titleHeight),CUSTOM_LIST_STYLE_NORMAL,true);
+	list->Construct(Rectangle(0, titleHeight, bounds.width, bounds.height
+			- titleHeight), CUSTOM_LIST_STYLE_NORMAL, true);
 	//list->Construct(Rectangle(0,0,bounds.width,bounds.height),CUSTOM_LIST_STYLE_NORMAL,true);
-	list->SetTextOfEmptyList("No results.");
+	list->SetTextOfEmptyList(empty);
 	list->AddTouchEventListener(*this);
 	AddControl(*list);
 
@@ -68,28 +82,30 @@ bool RoutePlannerResults::Initialize() {
 	 * Create context menu
 	 */
 	contextMenu = new ContextMenu;
-	contextMenu->Construct(Point(50,50), CONTEXT_MENU_STYLE_LIST);
-	contextMenu->AddItem(L"Save in Calendar", SAVE_CALENDAR_ACTION);
+	contextMenu->Construct(Point(50, 50), CONTEXT_MENU_STYLE_LIST);
+	contextMenu->AddItem(saveToCalendar, SAVE_CALENDAR_ACTION);
 	contextMenu->AddActionEventListener(*this);
 
 	//CREATE FORMAT
 	format = new CustomListItemFormat();
 	format->Construct();
-	format->AddElement(LIST_ITEM_TIMES,
-			Rectangle(0,0,0.6*width,0.5*height),0.5*height,Color::COLOR_WHITE,Color::COLOR_WHITE);
+	format->AddElement(LIST_ITEM_TIMES, Rectangle(0, 0, 0.6 * width, 0.5
+			* height), 0.5 * height, Color::COLOR_WHITE, Color::COLOR_WHITE);
 	//DELAY ELEMENT
-	format->AddElement(LIST_ITEM_DELAYS,
-			Rectangle(0.6*width,0,0.4*width,0.5*height),0.5*height,Color::COLOR_RED,Color::COLOR_RED);
+	format->AddElement(LIST_ITEM_DELAYS, Rectangle(0.6 * width, 0, 0.4 * width,
+			0.5 * height), 0.5 * height, Color::COLOR_RED, Color::COLOR_RED);
 	//date
-	format->AddElement(LIST_ITEM_DATE,
-			Rectangle(0.35*width,0.5*height,0.4*width,0.5*height),0.35*height,Color::COLOR_GREY,Color::COLOR_GREY);
+	format->AddElement(LIST_ITEM_DATE, Rectangle(0.35 * width, 0.5 * height,
+			0.4 * width, 0.5 * height), 0.35 * height, Color::COLOR_GREY,
+			Color::COLOR_GREY);
 	//DURATION
-	format->AddElement(LIST_ITEM_DURATION,
-			Rectangle(0,0.5*height,0.35*width,0.5*height),0.5*height,Color::COLOR_WHITE,Color::COLOR_WHITE);
+	format->AddElement(LIST_ITEM_DURATION, Rectangle(0, 0.5 * height, 0.35
+			* width, 0.5 * height), 0.5 * height, Color::COLOR_WHITE,
+			Color::COLOR_WHITE);
 	//Number of trains
-	format->AddElement(LIST_ITEM_NUMBER_CONNECTIONS,
-			Rectangle(0.75*width,0.5*height,0.25*width,0.5*height),0.35*height,Color::COLOR_WHITE,Color::COLOR_WHITE);
-
+	format->AddElement(LIST_ITEM_NUMBER_CONNECTIONS, Rectangle(0.75 * width,
+			0.5 * height, 0.25 * width, 0.5 * height), 0.35 * height,
+			Color::COLOR_WHITE, Color::COLOR_WHITE);
 
 	//CREATE SUBLISTFORMAT
 	subListFormat = new CustomListItemFormat();
@@ -100,26 +116,32 @@ bool RoutePlannerResults::Initialize() {
 	Color vehicleColor = Color::COLOR_GREY;
 
 	subListFormat->Construct();
-	subListFormat->AddElement(SUBLIST_FROM_TIME,
-			Rectangle(0,0,0.2*width,0.33*height),0.33*height,timeColor,timeColor);
-	subListFormat->AddElement(SUBLIST_FROM_DELAY,
-				Rectangle(0.2*width,0,0.2*width,0.33*height),0.33*height,delayColor,delayColor);
-	subListFormat->AddElement(SUBLIST_FROM_STATION,
-			Rectangle(0.4*width,0,0.50*width,0.33*height),0.33*height,stationColor,stationColor);
-	subListFormat->AddElement(SUBLIST_FROM_PLATFORM,
-			Rectangle(0.9*width,0,0.1*width,0.33*height),0.33*height,platformColor,platformColor);
+	subListFormat->AddElement(SUBLIST_FROM_TIME, Rectangle(0, 0, 0.2 * width,
+			0.33 * height), 0.33 * height, timeColor, timeColor);
+	subListFormat->AddElement(SUBLIST_FROM_DELAY, Rectangle(0.2 * width, 0, 0.2
+			* width, 0.33 * height), 0.33 * height, delayColor, delayColor);
+	subListFormat->AddElement(SUBLIST_FROM_STATION, Rectangle(0.4 * width, 0,
+			0.50 * width, 0.33 * height), 0.33 * height, stationColor,
+			stationColor);
+	subListFormat->AddElement(SUBLIST_FROM_PLATFORM, Rectangle(0.9 * width, 0,
+			0.1 * width, 0.33 * height), 0.33 * height, platformColor,
+			platformColor);
 
-	subListFormat->AddElement(SUBLIST_VEHICLE,
-			Rectangle(0.2*width,0.33*height,0.8*width,0.33*height),0.33*height,vehicleColor,vehicleColor);
+	subListFormat->AddElement(SUBLIST_VEHICLE, Rectangle(0.2 * width, 0.33
+			* height, 0.8 * width, 0.33 * height), 0.33 * height, vehicleColor,
+			vehicleColor);
 
-	subListFormat->AddElement(SUBLIST_TO_TIME,
-			Rectangle(0,0.66*height,0.2*width,0.33*height),0.33*height,timeColor,timeColor);
-	subListFormat->AddElement(SUBLIST_TO_DELAY,
-			Rectangle(0.2*width,0.66*height,0.2*width,0.33*height),0.33*height,delayColor,delayColor);
-	subListFormat->AddElement(SUBLIST_TO_STATION,
-			Rectangle(0.4*width,0.66*height,0.50*width,0.33*height),0.33*height,stationColor,stationColor);
-	subListFormat->AddElement(SUBLIST_TO_PLATFORM,
-			Rectangle(0.9*width,0.66*height,0.1*width,0.33*height),0.33*height,platformColor,platformColor);
+	subListFormat->AddElement(SUBLIST_TO_TIME, Rectangle(0, 0.66 * height, 0.2
+			* width, 0.33 * height), 0.33 * height, timeColor, timeColor);
+	subListFormat->AddElement(SUBLIST_TO_DELAY, Rectangle(0.2 * width, 0.66
+			* height, 0.2 * width, 0.33 * height), 0.33 * height, delayColor,
+			delayColor);
+	subListFormat->AddElement(SUBLIST_TO_STATION, Rectangle(0.4 * width, 0.66
+			* height, 0.50 * width, 0.33 * height), 0.33 * height,
+			stationColor, stationColor);
+	subListFormat->AddElement(SUBLIST_TO_PLATFORM, Rectangle(0.9 * width, 0.66
+			* height, 0.1 * width, 0.33 * height), 0.33 * height,
+			platformColor, platformColor);
 
 	return true;
 }
@@ -138,74 +160,86 @@ result RoutePlannerResults::OnTerminating(void) {
 /*
  * IAction Event Listener
  */
-void RoutePlannerResults::OnActionPerformed(const Osp::Ui::Control& source,int actionId) {
-	HeaderForm::OnActionPerformed(source,actionId);
-	if(actionId == REFRESH_ACTION){
+void RoutePlannerResults::OnActionPerformed(const Osp::Ui::Control& source,
+		int actionId) {
+	HeaderForm::OnActionPerformed(source, actionId);
+	if (actionId == REFRESH_ACTION) {
 		AppLog("Clicked RoutePlannerResults::refresh");
 
 		//show waiting popup with cancel interaction -> that then should go back to previous results
 		//when the results got back succesfull, only then the old results must be deleted and replaced by new
 		//this makes sure if we press cancel, we remain the previous results
 		Controller::GetInstance()->retrieveRoutePlannerResults();
-	}else if(actionId == BACK_ACTION){
+	} else if (actionId == BACK_ACTION) {
 		//goes back to the request. so we could edit the form we already made.
 		//in contrary to pressing on the header button route planner, that should give an empty form
 		AppLog("Clicked RoutePlannerResults::back");
 	}
 }
 
-
-void RoutePlannerResults::RequestRedraw (bool show) const{
+void RoutePlannerResults::RequestRedraw(bool show) const {
 	AppLog("RoutePlannerResults::RequestRedraw");
 	//load data
-	titleLabel->SetText(request->getFromStation()->getName() + " - " + request->getToStation()->getName());
+	titleLabel->SetText(request->getFromStation()->getName() + " - "
+			+ request->getToStation()->getName());
 
-	ArrayListT<Trip*>* trips = AppData::GetInstance()->getCurrentRequest()->getTrips();
-	Trip* trip=null;
+	ArrayListT<Trip*>* trips =
+			AppData::GetInstance()->getCurrentRequest()->getTrips();
+	Trip* trip = null;
 	list->RemoveAllItems();
-	for(int i=0;i<trips->GetCount();i++){
-		trips->GetAt(i,trip);
+	for (int i = 0; i < trips->GetCount(); i++) {
+		trips->GetAt(i, trip);
 		addTrip(trip);
 	}
 	Form::RequestRedraw(show);
 }
 
-void RoutePlannerResults::addTrip(Trip* trip)const{
+void RoutePlannerResults::addTrip(Trip* trip) const {
 	//GETTING THE DATA FROM THE TRIP
 	Connection* firstConn;
 	Connection* lastConn;
 	ArrayListT<Connection*>* connections = trip->getConnections();
-	connections->GetAt(0,firstConn);
-	connections->GetAt(connections->GetCount()-1,lastConn);
+	connections->GetAt(0, firstConn);
+	connections->GetAt(connections->GetCount() - 1, lastConn);
 	//Station names
 	/*
-	String stations;
-	stations += *firstConn->getStartNode()->getStation()->getName();
-	stations += L" - ";
-	stations += *lastConn->getEndNode()->getStation()->getName();
-	*/
+	 String stations;
+	 stations += *firstConn->getStartNode()->getStation()->getName();
+	 stations += L" - ";
+	 stations += *lastConn->getEndNode()->getStation()->getName();
+	 */
 	//Times
-	String times= Utils::formatTime(firstConn->getStartConnectionNode()->getDateTime());
-	times += L" - " + Utils::formatTime(lastConn->getEndConnectionNode()->getDateTime());
+	String times = Utils::formatTime(
+			firstConn->getStartConnectionNode()->getDateTime());
+	times += L" - " + Utils::formatTime(
+			lastConn->getEndConnectionNode()->getDateTime());
 	//DELAY
-	String delay=" ";
+	String delay = " ";
 	//if(firstConn->getStartConnectionNode()->getDelay() != null)
 	delay = Utils::formatDelay(firstConn->getStartConnectionNode()->getDelay());
 	//String delay = L"+0H25";
 	//DURATION
-	String duration= Utils::formatTime(trip->getDuration());
+	String duration = Utils::formatTime(trip->getDuration());
 	//DATE
-	String date= Utils::formatDate(firstConn->getStartConnectionNode()->getDateTime());
+	String date = Utils::formatDate(
+			firstConn->getStartConnectionNode()->getDateTime());
 
 	//number of trains
 	String numberOfTrains;
-	if(trip->getConnections()->GetCount() == 0) numberOfTrains= L"direct";
-	else numberOfTrains = L"#" + Integer::ToString(trip->getConnections()->GetCount());
+	String direct = "Direct";
+	AppResource* appRes = Application::GetInstance()->GetAppResource();
+	appRes->GetString(L"RP_RES_DIRECT", direct);
+
+	if (trip->getConnections()->GetCount() == 0)
+		numberOfTrains = direct;
+	else
+		numberOfTrains = L"#" + Integer::ToString(
+				trip->getConnections()->GetCount());
 	//CREATING ELEMENT
 	Rectangle bounds = this->GetClientAreaBounds();
-	int height = bounds.height/7;
-	if(bounds.width>bounds.height)
-		height = bounds.width /7;
+	int height = bounds.height / 7;
+	if (bounds.width > bounds.height)
+		height = bounds.width / 7;
 	CustomListItem * newItem = new CustomListItem();
 	newItem->Construct(height);
 	newItem->SetItemFormat(*format);
@@ -214,42 +248,48 @@ void RoutePlannerResults::addTrip(Trip* trip)const{
 	newItem->SetElement(LIST_ITEM_DELAYS, delay);
 	newItem->SetElement(LIST_ITEM_DURATION, duration);
 	newItem->SetElement(LIST_ITEM_NUMBER_CONNECTIONS, numberOfTrains);
-	newItem->SetElement(LIST_ITEM_DATE,date);
+	newItem->SetElement(LIST_ITEM_DATE, date);
 	int itemNumber = list->GetItemCount();
-	list->AddItem(*newItem,itemNumber);
+	list->AddItem(*newItem, itemNumber);
 
-	for(int i=0;i<trip->getConnections()->GetCount();i++){
+	for (int i = 0; i < trip->getConnections()->GetCount(); i++) {
 		Connection* conn = null;
-		trip->getConnections()->GetAt(i,conn);
+		trip->getConnections()->GetAt(i, conn);
 
-		String fromTime = Utils::formatTime(conn->getStartConnectionNode()->getDateTime());
-		String toTime = Utils::formatTime(conn->getEndConnectionNode()->getDateTime());
-		String fromDelay = Utils::formatDelay(conn->getStartConnectionNode()->getDelay());
-		String toDelay = Utils::formatDelay(conn->getEndConnectionNode()->getDelay());
+		String fromTime = Utils::formatTime(
+				conn->getStartConnectionNode()->getDateTime());
+		String toTime = Utils::formatTime(
+				conn->getEndConnectionNode()->getDateTime());
+		String fromDelay = Utils::formatDelay(
+				conn->getStartConnectionNode()->getDelay());
+		String toDelay = Utils::formatDelay(
+				conn->getEndConnectionNode()->getDelay());
 
-		String fromStation = String(conn->getStartConnectionNode()->getStation()->getName());
-		String toStation = String(conn->getEndConnectionNode()->getStation()->getName());
-		String fromPlatform = String(conn->getStartConnectionNode()->getPlatform());
+		String fromStation = String(
+				conn->getStartConnectionNode()->getStation()->getName());
+		String toStation = String(
+				conn->getEndConnectionNode()->getStation()->getName());
+		String fromPlatform = String(
+				conn->getStartConnectionNode()->getPlatform());
 		String toPlatform = String(conn->getEndConnectionNode()->getPlatform());
 
 		//SUBLIST_FROM_TIME,SUBLIST_TO_TIME,SUBLIST_FROM_STATION,SUBLIST_TO_STATION,SUBLIST_FROM_PLATFORM,SUBLIST_TO_PLATFORM
 		CustomListItem* subItem = new CustomListItem();
 		subItem->Construct(height);
 		subItem->SetItemFormat(*subListFormat);
-		subItem->SetElement(SUBLIST_FROM_TIME,fromTime);
-		subItem->SetElement(SUBLIST_FROM_DELAY,fromDelay);
-		subItem->SetElement(SUBLIST_FROM_STATION,fromStation);
-		subItem->SetElement(SUBLIST_FROM_PLATFORM,fromPlatform);
-		subItem->SetElement(SUBLIST_VEHICLE,conn->getVehicle()->getName());
-		subItem->SetElement(SUBLIST_TO_TIME,toTime);
-		subItem->SetElement(SUBLIST_TO_STATION,toStation);
-		subItem->SetElement(SUBLIST_TO_PLATFORM,toPlatform);
-		list->AddSubItem(itemNumber,*subItem,i);
+		subItem->SetElement(SUBLIST_FROM_TIME, fromTime);
+		subItem->SetElement(SUBLIST_FROM_DELAY, fromDelay);
+		subItem->SetElement(SUBLIST_FROM_STATION, fromStation);
+		subItem->SetElement(SUBLIST_FROM_PLATFORM, fromPlatform);
+		subItem->SetElement(SUBLIST_VEHICLE, conn->getVehicle()->getName());
+		subItem->SetElement(SUBLIST_TO_TIME, toTime);
+		subItem->SetElement(SUBLIST_TO_STATION, toStation);
+		subItem->SetElement(SUBLIST_TO_PLATFORM, toPlatform);
+		list->AddSubItem(itemNumber, *subItem, i);
 	}
 }
 
-void RoutePlannerResults::ShowContextMenu(bool show)
-{
+void RoutePlannerResults::ShowContextMenu(bool show) {
 	contextMenu->SetShowState(show);
 	if (show == true)
 		contextMenu->Show();
@@ -261,22 +301,43 @@ void RoutePlannerResults::ShowContextMenu(bool show)
 /*
  * ITouchEventListener
  */
-void RoutePlannerResults::OnTouchDoublePressed (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){}
-void RoutePlannerResults::OnTouchFocusIn (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){}
-void RoutePlannerResults::OnTouchFocusOut (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){}
-void RoutePlannerResults::OnTouchLongPressed (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){
+void RoutePlannerResults::OnTouchDoublePressed(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
+}
+void RoutePlannerResults::OnTouchFocusIn(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
+}
+void RoutePlannerResults::OnTouchFocusOut(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
+}
+void RoutePlannerResults::OnTouchLongPressed(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
 	if (&source == list) {
-		contextMenu->SetPosition(currentPosition.x, currentPosition.y+contextMenu->GetBounds().y);
+		contextMenu->SetPosition(currentPosition.x, currentPosition.y
+				+ contextMenu->GetBounds().y);
 		ShowContextMenu(true);
 		int sub = -1;
-		int mainIndex=-1;
-		list->GetItemIndexFromPosition(currentPosition,mainIndex,sub);
-		if(mainIndex >=0 )
+		int mainIndex = -1;
+		list->GetItemIndexFromPosition(currentPosition, mainIndex, sub);
+		if (mainIndex >= 0)
 			Controller::GetInstance()->saveToCalendar(mainIndex);
-			//AppLog("RoutePlannerResults::OnTouchLongPressed mainitem:%S",Integer::ToString(mainIndex).GetPointer());
+		//AppLog("RoutePlannerResults::OnTouchLongPressed mainitem:%S",Integer::ToString(mainIndex).GetPointer());
 	}
 }
-void RoutePlannerResults::OnTouchMoved (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){}
-void RoutePlannerResults::OnTouchPressed (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){}
-void RoutePlannerResults::OnTouchReleased (const Osp::Ui::Control &source, const Osp::Graphics::Point &currentPosition, const Osp::Ui::TouchEventInfo &touchInfo){}
+void RoutePlannerResults::OnTouchMoved(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
+}
+void RoutePlannerResults::OnTouchPressed(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
+}
+void RoutePlannerResults::OnTouchReleased(const Osp::Ui::Control &source,
+		const Osp::Graphics::Point &currentPosition,
+		const Osp::Ui::TouchEventInfo &touchInfo) {
+}
 
